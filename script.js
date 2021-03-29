c34cef37ca632104093f683ce2d5924256d44976
James Munger <jmunger@lakelandcollege.edu>
2021-03-29 18:43:10 +0000

    add example of on off animation, and back and forth animation

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+let position = 1;
 function move() {
-  var element = document.querySelector("#item1")
+  var element = document.querySelector("#item1");
   console.log(element.classList);
+  if(position == 1){
+    element.classList.replace("up", "down");
+    position *= -1;
+  }
+  else{
+    element.classList.replace("down", "up");
+    position *= -1;
+  }
 }
 
 function spin() {
-  var element = document.querySelector("#item1")
+  var element = document.querySelector("#item2");
+  element.classList.toggle("spin");
   console.log(element.classList);
 }
