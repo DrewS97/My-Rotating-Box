d6f144ba0c5a74901cbc584233fefa654796303d
James Munger <jmunger@lakelandcollege.edu>
2021-03-29 18:34:07 +0000

    add classes and functions for toggle and animations

---
+++ script.js
@@ -0,0 +1,9 @@
+function move() {
+  var element = document.querySelector("#item1")
+  console.log(element.classList);
+}
+
+function spin() {
+  var element = document.querySelector("#item1")
+  console.log(element.classList);
+}
