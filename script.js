3272890e69ad740fc52f201ad97c73ed6dda623b
dstoutin97 <drewstoutin97@gmail.com>
2021-04-01 21:36:41 +0000

    fix spin button and break move function.

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 let position = 1;
 function move() {
-  var element = document.querySelector("#item1");
-  console.log(element.classList);
-  if(position == 1){
-    element.classList.replace("up", "down");
-    position *= -1;
-  }
-  else{
-    element.classList.replace("down", "up");
-    position *= -1;
-  }
+  let boxes = document.querySelectorAll("div.item");
+  
+  boxes.forEach((e) => {
+    if(position == 1){
+      e.classList.toggle("up");
+      position *= -1;
+    }
+    else {
+      e.classList.toggle("down");
+      position *= -1;
+    }
+    
+    console.log(e.classList);
+  });
 }
 
 function spin() {
-  var element = document.querySelector("#item2");
-  element.classList.toggle("spin");
-  console.log(element.classList);
+  let matches = document.querySelectorAll("div.item");
+
+  matches.forEach((e) => {
+    if (e.classList == "up") {
+      e.classList.remove("up");
+    }
+    else if (e.classList == "down"){
+      e.classList.remove("down");
+    }
+    else {
+    e.classList.toggle("spin");
+    }
+    console.log(e.classList);
+  });
 }
