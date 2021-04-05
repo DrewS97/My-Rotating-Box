bc2a60134148618bbdd0e385df5d5c8dba1431e9
dstoutin97 <drewstoutin97@gmail.com>
2021-04-05 02:28:45 +0000

    fix redundancy

--- script.js
+++ script.js
@@ -1,34 +1,21 @@
-let position = 1;
+let boxList = document.querySelectorAll("div.item");
+
+boxList.forEach((b) => { 
+    b.addEventListener("click", move);
+});
+
 function move() {
-  let boxes = document.querySelectorAll("div.item");
-  
-  boxes.forEach((e) => {
-    if(position == 1){
-      e.classList.toggle("up");
-      position *= -1;
-    }
-    else {
-      e.classList.toggle("down");
-      position *= -1;
-    }
-    
-    console.log(e.classList);
+  boxList.forEach((b) => { 
+    b.classList.toggle("up");
+    b.classList.toggle("down");
+    console.log(b.classList);
   });
 }
 
 function spin() {
-  let matches = document.querySelectorAll("div.item");
-
-  matches.forEach((e) => {
-    if (e.classList == "up") {
-      e.classList.remove("up");
-    }
-    else if (e.classList == "down"){
-      e.classList.remove("down");
-    }
-    else {
-    e.classList.toggle("spin");
-    }
-    console.log(e.classList);
+  boxList.forEach((b) => { 
+    b.classList.remove("up", "down");
+    b.classList.toggle("spin");
+    console.log(b.classList);
   });
 }
